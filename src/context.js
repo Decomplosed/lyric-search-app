46a89a33f3e28f90f38b34d1246c12ebc1d44b65
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-25 18:25:55 +0100

    added value of whole state to Provider

--- src/context.js
+++ src/context.js
@@ -3,7 +3,34 @@ import React, { Component } from 'react'
 const Context = React.createContext()
 
 export class Provider extends Component {
+  state = {
+    trackList: [
+      {
+        track: {
+          track_name: 'abc'
+        }
+      },
+      {
+        track: {
+          track_name: '123'
+        }
+      },
+      {
+        track: {
+          track_name: '456'
+        }
+      }
+    ],
+    heading: 'Top 10 Tracks'
+  }
+
   render() {
-    return <Context.Provider>{this.props.children}</Context.Provider>
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    )
   }
 }
+
+export const Consumer = Context.Consumer
