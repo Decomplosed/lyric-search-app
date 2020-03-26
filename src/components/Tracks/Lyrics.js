845f4f199d16279ebaf161d2dfa49378a9d6befe
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-26 08:21:23 +0100

    server the error

--- src/components/Tracks/Lyrics.js
+++ src/components/Tracks/Lyrics.js
@@ -20,6 +20,7 @@ class Lyrics extends Component {
             `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
           )
           .then(res => this.setState({ track: res.data.message.body.track }))
+          .catch(err => console.log(err))
       })
       .catch(err => console.log(err))
   }
