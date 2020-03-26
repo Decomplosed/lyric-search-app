5b57e2af3db83545cb98e8b92e4bf375a62988ad
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-26 08:03:29 +0100

    fixed typo

--- src/components/Tracks/Track.js
+++ src/components/Tracks/Track.js
@@ -20,7 +20,7 @@ const Track = props => {
             : {track.album_name}
           </p>
           <Link
-            to={`lurics/track/${track.track_id}`}
+            to={`lyrics/track/${track.track_id}`}
             className='btn btn-dark btn-block'
           >
             <i className='fas fa-chevron-right'></i> View Lyrics
