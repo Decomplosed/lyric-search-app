b95c6b983bccce9ecf08bcccb3bcfbd7d9aa5d60
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-26 08:10:29 +0100

    fetch the data regarding lyrics

--- src/components/Tracks/Lyrics.js
+++ src/components/Tracks/Lyrics.js
@@ -7,7 +7,14 @@ class Lyrics extends Component {
     lyrics: {}
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
 
   render() {
     return (
