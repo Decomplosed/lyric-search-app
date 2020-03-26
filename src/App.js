f41344ebbae1d23b6d319e09283ac22443904af9
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-26 08:03:22 +0100

    created ROute for Lyrics

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <div className='container'>
             <Switch>
               <Route exact path='/' component={Index} />
+              <Route exact path='/lyrics/track/:id' component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
